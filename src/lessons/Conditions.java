97843fa3bc6718fc6b845290f2cb9c5aa5c29af8
e28166 <e28166@10.20.1.185>
2023-09-07 12:44:21 -0400

    s

---
+++ src/lessons/Conditions.java
@@ -0,0 +1,41 @@
+package lessons;
+
+public class Conditions {
+
+	public static void main(String[] args) {
+		
+		
+		/* conditions are comparisons
+		   The following are conditional operators:
+		   
+		   >
+		   <
+		   >=
+		   <=
+		   ==
+		   !=
+		  
+		 */
+		
+		int mark = 15;
+		
+		if (mark > 50)  {
+			
+			System.out.println("You passed!");
+		}
+		
+		if (mark < 50)  {
+			
+			System.out.println("You failed!");
+		}
+		
+		if (mark == 50)  {
+			
+			System.out.println("You just passed!");
+		}
+		
+	
+
+	}
+
+}
