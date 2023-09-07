168d2b3235c0387a840b176221c0d295becebbe2
e28166 <e28166@10.20.1.185>
2023-09-07 12:22:14 -0400

    s

---
+++ src/lessons/Variables.java
@@ -0,0 +1,31 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// variables
+		int favNum = 13;
+		double myDouble = 12.4;
+		int myInt;
+		char myChar = 'x';
+		String myString = "Hello World!";
+		boolean myBoolean = true;
+		
+		double mySum = favNum + myDouble;
+		
+		// constant variable (doesn't change)
+		final double TAX_RATE = 0.13;
+		
+		
+		// output
+		System.out.println(favNum);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myString);
+		System.out.println(myBoolean);
+		System.out.println(TAX_RATE);
+		System.out.println(mySum);
+	}
+
+}
