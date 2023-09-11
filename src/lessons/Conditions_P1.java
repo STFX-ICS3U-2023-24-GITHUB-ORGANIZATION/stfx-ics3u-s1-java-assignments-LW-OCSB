25a363b7c6a5fb3a0d04ae0f8d7598a69f7f4ffa
e28166 <e28166@10.20.1.185>
2023-09-11 09:01:20 -0400

    dsa

---
+++ src/lessons/Conditions_P1.java
@@ -0,0 +1,26 @@
+package lessons;
+
+public class Conditions_P1 {
+
+	public static void main(String[] args) {
+
+		int mark = 100;
+		
+		if (mark >= 50)  {
+			
+			System.out.println("You passed!");
+		}
+		
+		if (mark == 100) {
+			System.out.println("You got perfect!");
+		}
+		
+		if (mark != 0) {
+			System.out.println("You didn't get zero!");
+		}
+		
+		System.out.println("End of program.");
+
+	}
+
+}
