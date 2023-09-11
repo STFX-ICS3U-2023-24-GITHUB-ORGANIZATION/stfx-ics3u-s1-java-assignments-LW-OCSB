2ecb47a5ef84317acc9c706ff23e78a9e96efe7e
e28166 <e28166@10.20.1.185>
2023-09-11 09:11:35 -0400

    sdf

---
+++ src/lessons/IfElse.java
@@ -0,0 +1,22 @@
+package lessons;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+
+
+		int mark = 42;
+		
+		if (mark >= 50)  {
+			
+			System.out.println("You passed!");
+		}
+		
+		else  {
+			
+			System.out.println("You failed!");
+		}
+		
+	}
+
+}
