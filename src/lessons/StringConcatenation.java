495d33809be5699fa99e896a0e57fe59678db55d
e28166 <e28166@10.20.1.185>
2023-09-13 08:47:53 -0400

    s

---
+++ src/lessons/StringConcatenation.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		
+		String myString1 = "Hello ";
+		String myString2 = "World";
+		String myString3 = myString1 + myString2;
+		
+		System.out.println(myString3 + "!");
+	}
+
+}
