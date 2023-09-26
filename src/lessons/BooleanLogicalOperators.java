8d65a815ab140d8b0ddad99628043e34a9eec88d
e28166 <e28166@10.20.1.185>
2023-09-26 12:23:13 -0400

    ss

---
+++ src/lessons/BooleanLogicalOperators.java
@@ -0,0 +1,41 @@
+package lessons;
+
+public class BooleanLogicalOperators {
+
+	public static void main(String[] args) {
+		
+		if (true && true)  {
+			System.out.println("1");
+		}
+		
+		if (true && false)  {
+			System.out.println("2");
+		}
+		
+		if (false && true)  {
+			System.out.println("3");
+		}
+		
+		if (false && false)  {
+			System.out.println("4");
+		}
+		
+		if (true || true)  {
+			System.out.println("5");
+		}
+		
+		if (true || false)  {
+			System.out.println("6");
+		}
+		
+		if (false || true)  {
+			System.out.println("7");
+		}
+		
+		if (false || false)  {
+			System.out.println("8");
+		}
+
+	}
+
+}
--- src/lessons/OddEven.java
+++ src/lessons/OddEven.java
@@ -1,29 +1,30 @@
 package lessons;
+
 import java.util.Scanner;
 
-
-public class OddEven_P1 {
+public class OddEven {
 
 	public static void main(String[] args) {
-
+		
 		// variables
 		Scanner sc = new Scanner(System.in);
 		int userInput = 0;
 		
-		// prompt user for integer
+		// prompt user for number
 		System.out.println("Enter an integer: ");
 		userInput = sc.nextInt();
 		
-		// determine if integer is odd or even
-		if (userInput%2 > 0)  {
+		// determine if number is odd or even
+		if ( (userInput%2) > 0 )  {
 			
-			System.out.println("Integer is ODD.");
+			System.out.println("The number is ODD.");
 		}
 		
 		else  {
-			System.out.println("Integer is EVEN");
+			System.out.println("The number is EVEN");
 		}
 		
+
 	}
 
 }
