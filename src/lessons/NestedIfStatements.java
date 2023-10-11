8d5e99ae43472dccb20a8fe58a691faa23892c5c
e28166 <e28166@10.20.1.185>
2023-10-11 09:24:57 -0400

    s

---
+++ src/lessons/NestedIfStatements.java
@@ -0,0 +1,22 @@
+package lessons;
+import java.util.Scanner;
+
+public class NestedIfStatements {
+
+	public static void main(String[] args) {
+
+		// variables
+		Scanner sc = new Scanner(System.in);
+		String dietaryPreference	 = "";
+		
+		// Output
+		System.out.println("Food Menu");
+		System.out.println("Do you eat meat (M) or vegetables (V)?");
+		
+		// User input for dietary preference
+
+		
+		
+	}
+
+}
