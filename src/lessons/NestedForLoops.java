8a87bd1bc7533dae2ba03baac0b0374c8081a4d8
e28166 <e28166@10.20.1.185>
2023-10-23 12:34:33 -0400

    s

--- src/lessons/NestedForLoops.java
+++ src/lessons/NestedForLoops.java
@@ -9,15 +9,12 @@ public class NestedForLoops {
 			
 			System.out.println( "Outer loop number: " + (i+1));
 			
-			
 			// inner loop
 			for (int j=0; j<4; j++)  {
 				System.out.println( "Inner loop number: " + (j+1));
 			}
 			
 		}
-			
-		
 		
 	}
 
