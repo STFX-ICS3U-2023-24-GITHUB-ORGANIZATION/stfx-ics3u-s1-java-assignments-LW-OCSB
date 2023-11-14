45f17d32e23de3903a65ea1d7436a8a995e72616
e28166 <e28166@10.20.1.185>
2023-11-14 09:03:59 -0500

    s

--- src/lessons/UserDefinedMethods.java
+++ src/lessons/UserDefinedMethods.java
@@ -4,38 +4,41 @@ public class UserDefinedMethods {
 
 	public static void main(String[] args) {
 		
-		System.out.println(addThreeInts(1, 2, 3));
-		System.out.println(addFourDoubles(1.1, 2.2, 3.3, 4.4));
-		System.out.println(combineTwoStrings("foo ", "bar"));
+		System.out.println(addThreeInts(2, 5, 7));
+		System.out.println(addFourDoubles(1.1, 2.2, 3.3, 4.4));		
+		System.out.println(combineTwoStrings("fodsgfdsfgo ", "baadssdr"));
 		printHelloWorld();
 	}
 	
-	// DESCRIPTION - Adds 3 integers.
+	
+	// DESCRIPTION - Returns the sum of three integers.
 	// PARAMETERS - int x, int y, int z
 	// RETURN TYPE - int
-	public static int addThreeInts(int x, int y, int z) {
+	public static int addThreeInts(int x, int y, int z)  {
 		
-		return ( x + y + z );
+		return (x + y + z);
 	}
 	
-	// DESCRIPTION - Returns sum of four doubles.
+	
+	// DESCRIPTION - Returns the sum of four doubles.
 	// PARAMETERS - double d1, double d2, double d3, double d4
 	// RETURN TYPE - double
-	public static double addFourDoubles(double d1, double d2, double d3, double d4) {
-			
+	public static double addFourDoubles(double d1, double d2, double d3, double d4)  {
+		
 		return (d1 + d2 + d3 + d4);
 	}
+	
 	
 	// DESCRIPTION - Concatenates two Strings.
 	// PARAMETERS - String s1, String s2
 	// RETURN TYPE - String
-	public static String combineTwoStrings(String s1, String s2) {
-			
+	public static String combineTwoStrings(String s1, String s2)  {
+		
 		return (s1 + s2);
 	}
 	
-
-	// DESCRIPTION - Displays "Hello World!" to console
+	
+	// DESCRIPTION - Prints "Hello World!" to console.
 	// PARAMETERS - none
 	// RETURN TYPE - void
 	public static void printHelloWorld()  {
@@ -44,3 +47,4 @@ public class UserDefinedMethods {
 	}
 	
 }
+
